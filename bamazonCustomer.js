bd123bd652f25687524ddbd887e8bbf4c46d3cc2
Janet <Reid>
2019-12-03 18:22:02 +0800

    Initial files created

---
+++ bamazonCustomer.js
@@ -0,0 +1,48 @@
+var mysql = require ('mysql');
+var inquirer = require ('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "OliverHank6",
+    database: "bamazon"
+  });
+
+  //start connection to database
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected as id' + connection.threadId);
+  //call first function
+    purchaseProduct();
+  });
+
+  
+//display products in database table and then ask user which product they want to purchase
+  function purchaseProduct() {
+    connection.query('SELECT * FROM products', function(err, res){
+      if (err) throw err;
+      console.log(res);
+
+      //display products and price to user
+     for (var i = 0; i < res.length; i++) {
+       console.log('Product Id: ' + res[i].item_id + 'Item: ' + res[i].product_name +  ' | Department Name: ' + res.[i].department_name + ' | Price: ' + res[i].Price + ' | Stock: ' + res[i].stock_quantity);
+      };
+        inquirer.prompt({
+        name: "item_id",
+        type: "rawlist",
+        message: "Which item would you like to buy: ",
+        choices: function() {
+          var choiceArray = [];
+          for (var i = 0; i < res.length; i++){
+            choiceArray.push(res[i].product_name);
+          }
+            return choiceArray;
+          }
+        }
+        });
+      
+          
