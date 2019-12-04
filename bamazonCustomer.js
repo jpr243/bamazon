4aec1b3a1225b9cd0dd0d0f32c85d45d9a4c6b1b
Janet <Reid>
2019-12-04 18:56:51 +0800

    Update functions in customer file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,48 +1,68 @@
-var mysql = require ('mysql');
-var inquirer = require ('inquirer');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 3306,
-  
-    user: "root",
-  
-    password: "",
-    database: ""
-  });
+  host: "localhost",
 
-  //start connection to database
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log('connected as id' + connection.threadId);
+  port: 3306,
+
+  user: "root",
+
+  password: "OliverHank6",
+  database: "bamazon"
+});
+
+//start connection to database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id" + connection.threadId);
   //call first function
-    purchaseProduct();
-  });
+  displayProducts();
+});
 
-  
 //display products in database table and then ask user which product they want to purchase
-  function purchaseProduct() {
-    connection.query('SELECT * FROM products', function(err, res){
-      if (err) throw err;
-      console.log(res);
+var displayProducts = function() {
+  var selectProducts = "SELECT * FROM products";
+  connection.query(selectProducts, function(err, res) {
+    if (err) throw err;
+    var displayTable = new table({
+      head: ["Item ID", "Product Name", "Department", "Price", "Quantity"],
+      colWidths: [, 50, 50, 10, 10]
+    });
+    for (var i = 0; i < res.length; i++) {
+      displayTable.push([
+        res[i].item_id,
+        res[i].product_name,
+        res[i].department_name,
+        res[i].price,
+        res[i].stock_quantity
+      ]);
+    }
+    console.log(displayTable.toString());
+    purchasePrompt();
+  });
+};
 
-      //display products and price to user
-     for (var i = 0; i < res.length; i++) {
-       console.log('Product Id: ' + res[i].item_id + 'Item: ' + res[i].product_name +  ' | Department Name: ' + res.[i].department_name + ' | Price: ' + res[i].Price + ' | Stock: ' + res[i].stock_quantity);
-      };
-        inquirer.prompt({
+function purchasePrompt() {
+  inquirer
+    .prompt([
+      {
         name: "item_id",
-        type: "rawlist",
-        message: "Which item would you like to buy: ",
-        choices: function() {
-          var choiceArray = [];
-          for (var i = 0; i < res.length; i++){
-            choiceArray.push(res[i].product_name);
-          }
-            return choiceArray;
-          }
-        }
-        });
-      
-          
+        type: "input",
+        message: "Please enter ID of the product you would like to buy? ",
+        filter: Number
+      },
+      {
+        name: "stock_quantity",
+        type: "input",
+        message: "How many units do you wish to purchase?",
+        filter: Number
+      }
+    ])
+    .then(function(answers) {
+      var unitsNeeded = answers.stock_quantity;
+      var itemID = answers.item_id;
+      purchaseOrder(itemID, unitsNeeded);
+    });
+}
